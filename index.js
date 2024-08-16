98fac36bbe74874b6b09e2f874d563301b8e3171
naima-tushin <naima39tushin@gmail.com>
2024-08-16 20:38:07 +0600

    first commit

---
+++ index.js
@@ -0,0 +1,53 @@
+const express = require('express');
+const cors = require('cors');
+const { MongoClient, ServerApiVersion, ObjectId } = require('mongodb');
+require('dotenv').config()
+const app = express();
+const port = process.env.PORT || 5000;
+
+// middleware
+app.use(cors({
+    origin: ['http://localhost:5000', 'https://harvest-hub-client.web.app', 'http://localhost:5173', 'https://scic-job-task-tushin.web.app', 'https://harvest-hub-server-nine.vercel.app', 'http://localhost:5174'],
+}));
+app.use(express.json());
+
+
+const uri = `mongodb+srv://${process.env.DB_USER}:${process.env.DB_PASS}@cluster1.mj6vep2.mongodb.net/?retryWrites=true&w=majority&appName=Cluster1`;
+
+// Create a MongoClient with a MongoClientOptions object to set the Stable API version
+const client = new MongoClient(uri, {
+  serverApi: {
+    version: ServerApiVersion.v1,
+    strict: true,
+    deprecationErrors: true,
+  }
+});
+
+async function run() {
+  try {
+    // Connect the client to the server	(optional starting in v4.7)
+    // await client.connect();`
+
+    // Collection ID
+    const database = client.db("SCIC_Task")
+    const productCollection = database.collection("Products");
+
+    // Send a ping to confirm a successful connection
+    // await client.db("admin").command({ ping: 1 });
+    console.log("Pinged your deployment. You successfully connected to MongoDB!");
+  } catch (error) {
+    console.error('Error connecting to MongoDB:', error);
+  } finally {
+    // Ensures that the client will close when you finish/error
+    // await client.close();
+  }
+}
+run().catch(console.dir);
+
+app.get('/', (req, res) => {
+    res.send('SCIC Job Task Is Running')
+})
+
+app.listen(port, () => {
+    console.log(`SCIC Job Task Is Running On Port ${port}`)
+})
