70e94e4211be5b48ca4f4a56bbc9cb07503217f5
naima-tushin <naima39tushin@gmail.com>
2024-08-18 19:08:08 +0600

    Added get for users

--- index.js
+++ index.js
@@ -11,7 +11,6 @@ app.use(cors({
 }));
 app.use(express.json());
 
-
 const uri = `mongodb+srv://${process.env.DB_USER}:${process.env.DB_PASS}@cluster1.mj6vep2.mongodb.net/?retryWrites=true&w=majority&appName=Cluster1`;
 
 // Create a MongoClient with a MongoClientOptions object to set the Stable API version
